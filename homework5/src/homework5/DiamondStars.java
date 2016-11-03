8f9e3e98a299b2056c16e39cfd89cea62c49b41c
smpark222 <sungmin567@naver.com>
2016-11-04 00:33:17 +0900

    과제 5 완성

--- homework5/src/homework5/DiamondStars.java
+++ homework5/src/homework5/DiamondStars.java
@@ -1,10 +1,28 @@
 package homework5;
 public class DiamondStars {
 	void A04(){
+		System.out.print("다이아몬드의 크기 입력 : ");
 		String input = new ExamForWhileStars().input();
-		if (input.equalsIgnoreCase("q")) {
-			break;
-		}
 		int inputNum = Integer.parseInt(input);
+		
+		for (int i = 0; i < inputNum; i++) {
+			for (int j = 0; j < inputNum - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i * 2 + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = inputNum - 2; i >= 0; i--) {
+			for (int j = 0; j < inputNum - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i * 2 + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 	}
 }
--- homework5/src/homework5/IsosclesTriangleStars.java
+++ homework5/src/homework5/IsosclesTriangleStars.java
@@ -1,11 +1,20 @@
 package homework5;
 
 public class IsosclesTriangleStars {
-	void A03(){
+	void A03() {
+		System.out.print("이등변삼각형 높이 입력 : ");
 		String input = new ExamForWhileStars().input();
-		if (input.equalsIgnoreCase("q")) {
-			break;
-		}
 		int inputNum = Integer.parseInt(input);
+		for (int i = 0; i < inputNum; i++) {
+			for (int j = 0; j < inputNum - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i * 2 + 1; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println("");
+
+		}
 	}
 }
